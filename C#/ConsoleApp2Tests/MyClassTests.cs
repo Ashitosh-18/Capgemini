2f8865a3a12a0e25b783d98d50f2cc76fa42e08e
Ashitosh-18 <sheri.ashitosh@gmail.com>
2025-03-06 10:12:57 +0530

    initial commit

---
+++ C#/ConsoleApp2Tests/MyClassTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class MyClassTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            var myClass = new MyClass();
+
+            // Act
+            var result = myClass.Add(2, 3);
+
+            // Assert
+            Assert.AreEqual(5, result);
+        }
+    }
+}
