ac5b5d60bc6fd2afea907a7d715303b8385d9394
Ashitosh-18 <sheri.ashitosh@gmail.com>
2025-03-17 15:58:46 +0530

    commit

---
+++ C#/Practice2/2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+abstract class AbstractLogger
+{
+    public abstract void Log(string message);
+    public virtual void LogWarning(string message)
+    {
+        Console.WriteLine("Warning: " + message);
+    }
+}
+
+interface ILoggerInterface
+{
+    void Log(string message);
+}
+
+class FileLogger : AbstractLogger
+{
+    public override void Log(string message)
+    {
+        Console.WriteLine("Logging to file: " + message);
+    }
+}
---
+++ C#/Practice2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+interface ILogger
+{
+    void Save();
+}
+
+interface IDatabaseSaver
+{
+    void Save();
+}
+
+class DataManager : ILogger, IDatabaseSaver
+{
+    void ILogger.Save()
+    {
+        Console.WriteLine("Logging data...");
+    }
+
+    void IDatabaseSaver.Save()
+    {
+        Console.WriteLine("Saving data to database...");
+    }
+}
